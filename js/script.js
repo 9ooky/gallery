d216aaa650a33502ca8528b8be9a292954c8d9e7
9ooky <j1206047@gmail.com>
2019-05-15 18:03:27 +0900

    add vue.js and styles

---
+++ js/script.js
@@ -0,0 +1,32 @@
+new Vue({
+  el: '#app',
+  data: {
+    web_list: [{
+      id: 1,
+      url: 'https://www.ana.co.jp/group/',
+      name: 'ANAグループ企業情報',
+      image: 'ana.png',
+    },{
+      id: 2,
+      url: 'https://www.jal.com/ja/',
+      name: 'JAL企業サイト',
+      image: 'jal.png',
+    },{
+      id: 3,
+      url: 'https://www.tokyometro.jp/index.html',
+      name: '東京メトロ',
+      image: 'tokyometro.png',
+    },{
+      id: 4,
+      url: 'https://www.seiburailway.jp/',
+      name: '西武鉄道Webサイト',
+      image: 'seiburailway.png',
+    },{
+      id: 5,
+      url: 'https://www.kotsu.metro.tokyo.jp/',
+      name: '東京都交通局',
+      image: 'kotsu_metro_tokyo.png',
+    },
+    ]
+  }
+})
