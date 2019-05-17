a309e2313cf404b35b0c5e6eba2636bdad8d34fd
9ooky <j1206047@gmail.com>
2019-05-17 18:03:27 +0900

    add switch view function and content

--- js/script.js
+++ js/script.js
@@ -1,32 +1,70 @@
 new Vue({
   el: '#app',
   data: {
+    mode: 'pc',
     web_list: [{
-      id: 1,
       url: 'https://www.ana.co.jp/group/',
       name: 'ANAグループ企業情報',
-      image: 'ana.png',
+      image_pc: 'ana.png',
+      image_sp: 'ana_sp.png',
     },{
-      id: 2,
       url: 'https://www.jal.com/ja/',
       name: 'JAL企業サイト',
-      image: 'jal.png',
+      image_pc: 'jal.png',
+      image_sp: 'jal_sp.png',
     },{
-      id: 3,
+      url: 'https://www.jreast.co.jp/',
+      name: 'JR東日本',
+      image_pc: 'jreast.png',
+      image_sp: 'jreast_sp.png',
+    },{
       url: 'https://www.tokyometro.jp/index.html',
       name: '東京メトロ',
-      image: 'tokyometro.png',
+      image_pc: 'tokyometro.png',
+      image_sp: 'tokyometro_sp.png',
     },{
-      id: 4,
       url: 'https://www.seiburailway.jp/',
       name: '西武鉄道Webサイト',
-      image: 'seiburailway.png',
+      image_pc: 'seiburailway.png',
+      image_sp: 'seiburailway_sp.png',
     },{
-      id: 5,
       url: 'https://www.kotsu.metro.tokyo.jp/',
       name: '東京都交通局',
-      image: 'kotsu_metro_tokyo.png',
-    },
-    ]
+      image_pc: 'kotsu_metro_tokyo.png',
+      image_sp: 'kotsu_metro_tokyo_sp.png',
+    },{
+      url: 'https://www.asahibeer.co.jp/',
+      name: 'アサヒビール',
+      image_pc: 'asahibeer.png',
+      image_sp: 'asahibeer_sp.png',
+    },{
+      url: 'https://www.meiji.com/',
+      name: '明治ホールディングス株式会社',
+      image_pc: 'meiji.png',
+      image_sp: 'meiji_sp.png',
+    },{
+      url: 'https://www.morinaga.co.jp/',
+      name: '森永製菓株式会社',
+      image_pc: 'morinaga.png',
+      image_sp: 'morinaga_sp.png',
+    },{
+      url: 'https://www.suntory.co.jp/',
+      name: 'サントリー ホームページ',
+      image_pc: 'suntory.png',
+      image_sp: 'suntory_sp.png',
+    },{
+      url: 'https://www.kao.com/jp/',
+      name: 'Kao 花王株式会社',
+      image_pc: 'kao.png',
+      image_sp: 'kao_sp.png',
+    },{
+      url: 'https://www.kanebo-cosmetics.co.jp/',
+      name: 'カネボウ化粧品',
+      image_pc: 'kanebo.png',
+      image_sp: 'kanebo_sp.png',
+    }
+  ],
+  },
+  methods: {
   }
 })
